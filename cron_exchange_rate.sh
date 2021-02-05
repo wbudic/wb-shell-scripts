841d9448b33053c2c24e17105f05c4a1988c4504
wbudic <42565654+wbudic@users.noreply.github.com>
2021-02-05 12:17:43 +1100

    Update cron_exchange_rate.sh

--- cron_exchange_rate.sh
+++ cron_exchange_rate.sh
@@ -3,7 +3,7 @@ export XDG_RUNTIME_DIR=/run/user/$(id -u)
 export DISPLAY=":0"
 export XAUTHORITY="$HOME/.Xauthority"
 
-grab=$(curl -X GET https://openexchangerates.org/api/latest.json?app_id=00a1943886e24f9bb240e6aa3f7b9868) 
+grab=$(curl -X GET https://openexchangerates.org/api/latest.json?app_id=INSTALL_YOURS) 
 echo $grab
 ~/uvar.sh EXCHANGE_JSON '$grab'
 ~/uvar.sh EXCHANGE_AUD $(echo $grab | jq '.rates.AUD')
