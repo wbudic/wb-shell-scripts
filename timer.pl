6254ef4b7616ef6ad32c61d4af7886bd981339f3
wbudic <vilijanac@gmail.com>
2021-06-13 06:36:45 +1000

    Best free countdown timer in the world.

--- timer.pl
+++ timer.pl
@@ -3,36 +3,59 @@ use strict;
 use warnings;
 use Term::ANSIColor qw(:constants);
 use Term::Screen;
-use Term::Menus;
 
-my $cnt_down = 15*60; # in secs, default is 15 min.
-my $beg = time;
-my $end = $beg + $cnt_down;
+
+my $sta = time;
+my $end = $sta + 15*60;
 my ($msg, $time, $cur, $cmd); 
 
  if(scalar @ARGV>0){
    foreach my $arg(@ARGV){
       if ($arg =~ /\d+/){
-            $end = $beg + $arg * 60; print "Minutes -> $arg\n";
+            $end = $sta + $arg * 60; print "Minutes -> $arg\n";
       }else{
-            $cmd = $arg; print "Command -> $cmd\n";
+
+            if($arg =~ m/^-+.*/){
+               while(<DATA>){print $_}
+               exit;
+            }
+            else{
+               $cmd = $arg; print "Command -> $cmd\n";
+            }
       }
    }
    print "Activating...\n";
  }else{
-    my @lst = (5,15,20,30,40,45,50,60);
-    my $sel=&pick(\@lst,"Which is your desired timeout in minutes (from 1..8)?");
-		if($sel ne "]quit["){
-         print "Setting timer to $sel minutes.\n"; 
-         sleep(1);
-         $beg = time;
-	      $end = $beg + $sel * 60;
+    my $lst = 'Cancel\n 3m\n 5m\n 10m\n 15m\n 20m\n 25m\n 30m\n 40m\n 45m\n 50m\n 60m\n 90m\n 1h\n 2h\n 3h\n 4h\n 3s\n 10s\n 30s';
+    my $cmd = qq(
+       echo '$lst'|fzf --no-multi --cycle --height=12 --border --margin 1,1% --pointer='->' --color=dark --print-query
+                          --info=inline --header='<- Please select your desired timeout. Check out your, ^ arrow, PgUp, PgDwn, and ESC key.'
+    ); $cmd =~ s/[\n\r\t]//g; 
+    my $sel=`$cmd`;$sel =~ s/\s//g; 
+		if($sel ne "Cancel"){
+         $sta = time;
+         if($sel =~ m/m$/){
+            $sel =~ s/m$//;
+            print "Setting timer to [$sel] minutes.\n";
+            $end = $sta + $sel * 60;
+         }
+         elsif($sel =~ m/h$/){
+            print "Setting timer to $sel.\n";
+            $sel =~ s/h$//;
+            $sel *= 60;
+            $end = $sta + $sel * 60;
+         }elsif($sel =~ m/s$/){
+            $sel =~ s/s$//;
+            $end = $sta + $sel;
 
+         }else{
+            exit 1;#<- should not be possible!
+         }
+         sleep(1);   
 		}else{
 			 exit; 
 		}
  }
-
 
 my $t = Term::Screen->new();
 my $exited = 0;
@@ -43,7 +66,9 @@ $SIG{'INT'} = sub {
 	$exited=1; 
 };
 $| = 1;
-$t->noecho(); $t->at($t->rows()-2,0); 
+$t->noecho(); 
+$t->at($t->rows()-2,0); 
+
 for (;;) {
    $time = time;
    last if ($time >= $end);
@@ -67,8 +92,21 @@ $t->echo(); $t->curvis(); undef $t;
 if($cmd){
  system($cmd);
 }else{
- system('mpv --no-video "$ENV{HOME}/Fiona Apple - Fast As You Can-NbxqtbqyoRk.mkv"');
+ system(qq(mpv --no-video '$ENV{HOME}/Fiona Apple - Fast As You Can-NbxqtbqyoRk.mkv'));
 }
+__END__
+COUNTDOWN TIMER
+  by Will Budic (https://github.com/wbudic/wb-shell-scripts)
 
+Syntax:
 
-1;
+$ ./timer.pl {minutes} {shell_cmd}
+
+Without any arguments, uses the fzf utility to propmt for period.
+Defaults to 15 minutes, if only shell command is given.
+Modify last line in script to include own alarm/music to play as default on timer expiry.
+System bells are unacceptable.
+
+Requirements:
+sudo apt install mvp
+fzf (don't use apt to install, can be crap, install manually for vim and from your home)
