28a2fd964d0922305af4d580797d72852a59deaa
wbudic <vilijanac@gmail.com>
2021-11-02 12:29:17 +1100

    exposed appid fix.

--- cron_weather.sh
+++ cron_weather.sh
@@ -5,7 +5,7 @@ export XAUTHORITY="$HOME/.Xauthority"
 
 echo -e $(date +"%D %T") $(basename $0) "Started"
 
-grab=$(curl -X GET 'https://api.openweathermap.org/data/2.5/weather?q=Sydney,NSW,AU&units=metric&appid=2fc5554698ddb34d6a9eedec75b01e2f')
+grab=$(curl -X GET 'https://api.openweathermap.org/data/2.5/weather?q=Sydney,NSW,AU&units=metric&appid=xxx')
 json=$(echo $grab | jq '{City: .name, Weather:.weather[].description, Temp: .main.temp}')
 echo $json
 formated=$(echo $json | jq -c -j '(.City+" - "+.Weather+ " ", .Temp, " C°\n")') 
