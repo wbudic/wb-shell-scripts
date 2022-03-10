4fecfa3ad668148daa30078d8090a3c5430aa0a7
wbudic <vilijanac@gmail.com>
2022-03-10 18:14:28 +1100

    jumping to Videos dir now and back automatically.

--- get_flash_video.sh
+++ get_flash_video.sh
@@ -1,6 +1,8 @@
  
 #!/bin/bash
+pushd .; cd ~/Videos
 youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' $1
+popd
 #Requirements:
 #sudo apt install youtube-dl
 #sudo apt install python3-q-text-as-data ffmpeg
