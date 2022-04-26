207eb080b5a7842238f1597ba762d32ed2361e42
Will Budic <vilijanac@gmail.com>
2022-04-27 07:23:32 +1000

    switched to yt-dlp

--- get_flash_video.sh
+++ get_flash_video.sh
@@ -1,8 +1,11 @@
  
 #!/bin/bash
 pushd .; cd ~/Videos
-youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' $1
+#youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' $1
+yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' $1
 popd
 #Requirements:
-#sudo apt install youtube-dl
+#sudo apt install python3-pip
+#python3 -m pip install -U yt-dlp
 #sudo apt install python3-q-text-as-data ffmpeg
+#
